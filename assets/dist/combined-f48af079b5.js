4dc5a0fdbcf3a922222146a9d65fdedc83e5efea
siddharth_mantri <siddharthmantri1@gmail.com>
2015-12-08 04:25:47 -0500

    Updated search bars

--- assets/dist/combined-f48af079b5.js
+++ assets/dist/combined-f48af079b5.js
@@ -19,10 +19,33 @@ npwmApp.controller('indexCtrl', ['$scope', '$http', '$rootScope', '$state',
 		}
 	}
 ])
-npwmApp.controller('restaurantCtrl', ['$scope', '$http', '$rootScope', '$stateParams',
-	function($scope, $http, $rootScope, $stateParams){
+npwmApp.controller('restaurantCtrl', ['$scope', '$http', '$rootScope', '$stateParams', '$state',
+	function($scope, $http, $rootScope, $stateParams, $state){
 		// $rootScope.serverUrl = nodeUrl.url;
 		$scope.comment = {}
+		$scope.serverList = [pythonUrl, nodeUrl]
+		$scope.getCuisines = function(){
+			$http.get($rootScope.serverUrl+'/cuisines').
+			success(function(data){
+				$scope.cuisines = data.response;	
+			})
+		}
+		if($rootScope.serverUrl){
+			for(f in $scope.serverList){
+				if($rootScope.serverUrl == f.url){
+					$scope.selectedServer = f;
+				}
+			}
+		}
+		$scope.setServer = function(flag){
+			$rootScope.serverUrl = flag == 2 ? nodeUrl.url:pythonUrl.url;
+			$scope.selectedServer = flag == 2 ? nodeUrl:pythonUrl;
+			$scope.getCuisines()
+		}
+		$scope.invokeSearch = function(){
+			q = $scope.asyncSelected.replace(/ /g, '-');
+			$state.go('search', {query: q});
+		}
 		$http.get($rootScope.serverUrl+'/restaurant/'+$stateParams.id).
 		success(function(data){
 			if(data.success == true){
